73be8df77c19e0c95d2ce4804ec3963cd607ad2a
Malik usman Tariq <malikusmantariq@Maliks-MacBook-Pro.local>
2021-05-20 16:28:13 +0100

    Initial commit

---
+++ src/sample/PackingMachine.java
@@ -0,0 +1,45 @@
+package sample;
+
+public class PackingMachine
+{
+
+
+    private int bags;
+
+    private final int Maximumtimeinminutes = 60;
+
+
+
+    private void add_Ten_Treats()
+    {
+
+    }
+
+
+    private void add_Twenty_Treats()
+    {
+
+    }
+
+
+    private void add_Fifty_Treats()
+    {
+
+    }
+
+
+    private void machine_Clearning()
+    {
+
+    }
+
+
+
+    private void bags_Restocking()
+    {
+
+    }
+
+
+
+}
---
+++ src/sample/Treat.java
@@ -0,0 +1,27 @@
+package sample;
+
+public class Treat
+{
+
+
+
+
+
+    private void treat_Producing ()
+    {
+
+    }
+
+
+    private void treat_Produced ()
+    {
+
+    }
+
+
+    private void tetreive_Treat ()
+    {
+
+    }
+
+}
